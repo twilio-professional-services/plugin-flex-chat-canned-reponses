e6941a462673b82ff02184a11f862eabf5e47a23
Mike Schmitt <mikeschmitt@c02gkaqdmd6m.lan>
2022-04-26 11:14:40 -0500

    Twilio CLI Function & deployment

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,8 @@ import { useQuery } from 'react-query';
 import axios from 'axios';
 
 const getCannedReponses = async () => {
-    const { data } = await axios.get('https://plugin-chat-responses-data-8720.twil.io/responses');
+    const endpointURL = process.env.REACT_APP_SERVICE_FUNCTION_URL ?? '';
+    const { data } = await axios.get(endpointURL);
     return data;
 }
 
